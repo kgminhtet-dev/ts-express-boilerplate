1b507187ae709c5fbe2dac97c05c771eddaa0cac
mr-kmh <kgminhtet.dev@gmail.com>
2024-06-17 16:23:27 +0630

    add modules and fix errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(helmet());
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
